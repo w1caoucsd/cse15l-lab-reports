7bd6c8372aefb2654cc50159daa2a5fca00f2a9d
w1caoucsd <w1cao@ucsd.edu>
2022-04-26 14:05:28 -0700

    Modify markdown Parse to accommodate faulty test files

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,24 +4,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        ArrayList<String> input = new ArrayList<>(Arrays.asList(markdown.split("\n")));
+        System.out.println(input);
+        for (int i=0;i<input.size();i++){
+            int openBracket = input.get(i).indexOf("[");
+            int closeBracket = input.get(i).indexOf("]");
+            int openParen = input.get(i).indexOf("(");
+            int closeParen = input.get(i).indexOf(")");
+            if(openBracket!=-1&&closeBracket!=-1&&openParen!=-1&&closeParen!=-1){
+                String temp= input.get(i).substring(openParen + 1, closeParen);
+                toReturn.add(temp);
+            }
+        }
         // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        //add a comment 
+        /*int currentIndex = 0; 
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            String temp= markdown.substring(openParen + 1, closeParen);
+            if(toReturn.contains(temp)){
+            }
+            else{
+                toReturn.add(temp);
+            }
+            currentIndex++;
             System.out.println(currentIndex);
-        }
-
+        }*/
         return toReturn;
     }
 
